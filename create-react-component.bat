5f869a56be21ea0d4a9bcf8c83d73a44c481234d
Ryan Wright <SivanoFFDS@gmail.com>
2020-10-23 14:33:00 +0700

    Fixes

--- create-react-component.bat
+++ create-react-component.bat
@@ -1,33 +1,30 @@
 @echo off
-set /p folder=Enter component folder [folder\/]:%=%
-if exist .\src\components\%folder% (
-  set /p componentName=Enter component name:%=%
-  md "%componentName%"
-  cd "%componentName%"
-  echo import React from ^'react^'^;> .\src\conponents\%folder%%componentName%.ts
-  echo. >> .\src\conponents\%folder%%componentName%.ts
+set /p componentName=Enter component name:%=%
+cd ./src/components
+md %componentName%
+cd ./%componentName%
 
-  set /p needStyles=Need style? [y/n]:%=%
-  if /i "%needStyles%"=="y" (
-  echo import ^'./%componentName%.scss^';>> .\src\conponents\%folder%%componentName%.ts
-  echo. >> .\src\conponents\%folder%%componentName%.ts
-  )
+echo import React from ^'react^'^;>%componentName%.ts
+echo.>>%componentName%.ts
 
-  echo const %componentName%: React.FC = ^(^) =^> {>> .\src\conponents\%folder%%componentName%.ts
-  echo   return (>> .\src\conponents\%folder%%componentName%.ts
-  echo     ^<^>>> .\src\conponents\%folder%%componentName%.ts
-  echo. >> .\src\conponents\%folder%%componentName%.ts
-  echo     ^</^>>> .\src\conponents\%folder%%componentName%.ts
-  echo   ^)^;>> .\src\conponents\%folder%%componentName%.ts
-  echo }^;>> .\src\conponents\%folder%%componentName%.ts
-  echo. >> .\src\conponents\%folder%%componentName%.ts
-  echo export default %componentName%;>> .\src\conponents\%folder%%componentName%.ts
-  echo. >> .\src\conponents\%folder%%componentName%.ts
-
-  echo export ^{ default as %componentName% ^} from '.^/%componentName%';>.\src\conponents\%folder%index.ts
-
-  if /i "%needStyles%"=="y" echo. >.\src\conponents\%folder%%componentName%.scss
-) else (
-  echo Path .\src\components\%folder% doesn't exist
-  pause
+set /p needStyles=Need style? [y/n]:%=%
+if /i "%needStyles%"=="y" (
+echo import ^'./%componentName%.scss^';>>%componentName%.ts
+echo.>>%componentName%.ts
 )
+
+echo const %componentName%: React.FC = ^(^) =^> {>>%componentName%.ts
+echo   return (>>%componentName%.ts
+echo     ^<^>>>%componentName%.ts
+echo.>>%componentName%.ts
+echo     ^</^>>>%componentName%.ts
+echo   ^)^;>>%componentName%.ts
+echo }^;>>%componentName%.ts
+echo export default %componentName%;>>%componentName%.ts
+
+echo export ^{ default as %componentName% ^} from '.^/%componentName%';>index.ts
+echo.>>index.ts
+
+if /i "%needStyles%"=="y" echo.>%componentName%.scss
+
+cd ../../..
