20aa7c3594c36517cfe4302b2244f939511b8643
Ryan Wright <SivanoFFDS@gmail.com>
2020-10-29 18:07:55 +0700

    cmd arguments provided

--- create-react-component.bat
+++ create-react-component.bat
@@ -1,26 +1,34 @@
 @echo off
+
+if /i "%1"=="" (
 set /p componentName=Enter component name:%=%
-cd ./src/components
+) else set componentName=%1
+
+if /i "%2"=="" (
+set componentPath="components"
+) else set componentPath="%2"
+
+cd ./src/%componentPath%
 md %componentName%
 cd ./%componentName%
 
-echo import React from ^'react^'^;>%componentName%.ts
-echo.>>%componentName%.ts
+echo import React from ^'react^'^;>%componentName%.tsx
+echo.>>%componentName%.tsx
 
 set /p needStyles=Need style? [y/n]:%=%
 if /i "%needStyles%"=="y" (
-echo import ^'./%componentName%.scss^';>>%componentName%.ts
-echo.>>%componentName%.ts
+echo import ^'./%componentName%.scss^';>>%componentName%.tsx
+echo.>>%componentName%.tsx
 )
 
-echo const %componentName%: React.FC = ^(^) =^> {>>%componentName%.ts
-echo   return (>>%componentName%.ts
-echo     ^<^>>>%componentName%.ts
-echo.>>%componentName%.ts
-echo     ^</^>>>%componentName%.ts
-echo   ^)^;>>%componentName%.ts
-echo }^;>>%componentName%.ts
-echo export default %componentName%;>>%componentName%.ts
+echo const %componentName%: React.FC = ^(^) =^> {>>%componentName%.tsx
+echo   return (>>%componentName%.tsx
+echo     ^<^>>>%componentName%.tsx
+echo.>>%componentName%.tsx
+echo     ^</^>>>%componentName%.tsx
+echo   ^)^;>>%componentName%.tsx
+echo }^;>>%componentName%.tsx
+echo export default %componentName%;>>%componentName%.tsx
 
 echo export ^{ default as %componentName% ^} from '.^/%componentName%';>index.ts
 echo.>>index.ts
